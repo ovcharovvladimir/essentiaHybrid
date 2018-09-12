d56f4644955fd5dd87c627956809a85c08b0f906
Vladimir Ovcharov <vladimirovcharov@essentia.one>
2018-09-12 12:01:09 -0400

    Update Makefile
    added required packages
    updates .travis.yml

--- build/env.sh
+++ build/env.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 set -e
 
@@ -9,22 +9,166 @@ fi
 
 # Create fake Go workspace if it doesn't exist yet.
 workspace="$PWD/build/_workspace"
+if [[ $PWD = *"travis"* ]];
+then
+    solution="$GOPATH/go/src"
+    #"${PWD%/go/src/*}"
+else
+    solution="${PWD%/go/src/*}/go/src"
+fi
+
+#"$GOPATH"
 root="$PWD"
-essdir="$workspace/src/github.com/ovcharovvladimir"
-if [ ! -L "$essdir/essentiaHybrid" ]; then
-    mkdir -p "$essdir"
-    cd "$essdir"
+echo "GOPATH: $GOPATH"
+echo "Project Working Dir: $PWD"
+echo "Workspace: $workspace"
+echo "Solution: $solution"
+dir="$workspace/src/github.com/ovcharovvladimir"
+
+
+#"$GOPATH"
+root="$PWD"
+echo "GOPATH: $GOPATH"
+echo "Project Working Dir: $PWD"
+echo "Workspace: $workspace"
+echo "Solution: $solution"
+dir="$workspace/src/github.com/ovcharovvladimir"
+ 
+
+
+
+if [ ! -L "$dir/essentiaHybrid" ]; then
+    mkdir -p "$dir"
+    cd "$dir"
     ln -s ../../../../../. essentiaHybrid
     cd "$root"
 fi
+pth="$workspace/src/github.com/"
+#github.com/dancannon/gorethink
+if [ ! -L "$pth/dancannon" ]; then
+    mkdir -p "$pth"
+    cd "$pth"
+    ln -s $solution/github.com/dancannon/. dancannon
+    cd "$root"
+fi
+#github.com/sirupsen/logrus
+pth="$workspace/src/github.com/"
 
-# Set up the environment to use the workspace.
+if [ ! -L "$pth/sirupsen" ]; then
+    mkdir -p "$pth"
+    cd "$pth"
+    ln -s $solution/github.com/sirupsen/. sirupsen
+    cd "$root"
+fi
+#github.com/x-cray/logrus-prefixed-formatter
+pth="$workspace/src/github.com"
+
+if [ ! -L "$pth/x-cray" ]; then
+    mkdir -p "$pth"
+    cd "$pth"
+    ln -s $solution/github.com/x-cray/. x-cray
+    cd "$root"
+fi
+#
+#github.com/cenkalti/backoff
+pth="$workspace/src/github.com"
+
+if [ ! -L "$pth/cenkalti" ]; then
+    mkdir -p "$pth"
+    cd "$pth"
+    ln -s $solution/github.com/cenkalti/. cenkalti
+    cd "$root"
+fi
+#github.com/opentracing/opentracing-go
+pth="$workspace/src/github.com"
+
+if [ ! -L "$pth/opentracing" ]; then
+    mkdir -p "$pth"
+    cd "$pth"
+    ln -s $solution/github.com/opentracing/. opentracing
+    cd "$root"
+fi
+#gopkg.in/rethinkdb
+pth="$workspace/src/gopkg.in"
+
+if [ ! -L "$pth/rethinkdb" ]; then
+    mkdir -p "$pth"
+    cd "$pth"
+    ln -s $solution/gopkg.in/rethinkdb/. rethinkdb
+    cd "$root"
+fi
+#gopkg.in/fatih/pool.v2
+pth="$workspace/src/gopkg.in"
+
+if [ ! -L "$pth/fatih" ]; then
+    mkdir -p "$pth"
+    cd "$pth"
+    ln -s $solution/gopkg.in/fatih/. fatih
+    cd "$root"
+fi
+#gopkg.in/rethinkdb/rethinkdb-go.v5/encoding
+pth="$workspace/src/gopkg.in"
+
+if [ ! -L "$pth/rethinkdb" ]; then
+    mkdir -p "$pth"
+    cd "$pth"
+    ln -s $solution/gopkg.in/rethinkdb/rethinkdb-go.v5/. rethinkdb-go.v5
+    cd "$root"
+fi
+#golang.org/x/sys
+pth="$workspace/src/golang.org"
+
+if [ ! -L "$pth/x" ]; then
+    mkdir -p "$pth"
+    cd "$pth"
+    ln -s $solution/golang.org/x/. x
+    cd "$root"
+fi
+#github.com/mgutz/ansi
+pth="$workspace/src/github.com"
+
+if [ ! -L "$pth/mgutz" ]; then
+    mkdir -p "$pth"
+    cd "$pth"
+    ln -s $solution/github.com/mgutz/. mgutz
+    cd "$root"
+fi
+#github.com/mattn/go-colorable
+pth="$workspace/src/github.com"
+
+if [ ! -L "$pth/mattn" ]; then
+    mkdir -p "$pth"
+    cd "$pth"
+    ln -s $solution/github.com/mattn/. mattn
+    cd "$root"
+fi
+#github.com/golang/protobuf
+pth="$workspace/src/github.com"
+
+if [ ! -L "$pth/golang" ]; then
+    mkdir -p "$pth"
+    cd "$pth"
+    ln -s $solution/github.com/golang/. golang
+    cd "$root"
+fi
+#github.com/hailocab/go-hostpool
+pth="$workspace/src/github.com"
+
+if [ ! -L "$pth/hailocab" ]; then
+    mkdir -p "$pth"
+    cd "$pth"
+    ln -s $solution/github.com/hailocab/. hailocab
+    cd "$root"
+fi
+
+
 GOPATH="$workspace"
-export GOPATH
+export GOPATH 
+
 
 # Run the command inside the workspace.
-cd "$essdir/essentiaHybrid"
-PWD="$essdir/essentiaHybrid"
-
+cd "$dir/essentiaHybrid"
+PWD="$dir/essentiaHybrid"
+echo "----- $@"
 # Launch the arguments with the configured environment.
 exec "$@"
