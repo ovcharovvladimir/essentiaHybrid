d42358d6cffaaacd54d4d3bb22f4dc36171e915c
essarthur <essarthur@essentia.one>
2018-09-14 17:44:00 +0300

    Add log procedure to worker and blockchain

--- logserv/vars.go
+++ logserv/vars.go
@@ -7,8 +7,8 @@ import (
 
 //************************************************************
 //  Name    : Gloabal Variables for connection 
-//  Date    : is a snippet.
-//  Author  : Svachenko Arthr
+//  Date    : 10-09-2018
+//  Author  : Svachenko Arthur
 //  Company : Essentia
 //  Number  : 
 //  Module  : 
@@ -30,7 +30,7 @@ var (
 //************************************************************
 //  Name    : Base Type 
 //  Date    : 10-09-2018
-//  Author  : Svachenko Arthr
+//  Author  : Svachenko Arthur
 //  Company : Essentia
 //  Number  : 
 //  Module  : Declaration inetrfaces & structure & other type
@@ -48,7 +48,7 @@ type Mil []int64
 //************************************************************
 //  Name    : Structure monitor data 
 //  Date    : 09-10-2018
-//  Author  : Svachenko Arthr
+//  Author  : Svachenko Arthur
 //  Company : Essentia
 //  Number  : 
 //  Module  : 
@@ -78,7 +78,7 @@ func (L *LogStruct) Sys(){
 //************************************************************
 //  Name    : Base vars global level 
 //  Date    : 09-10-2018
-//  Author  : Svachenko Arthr
+//  Author  : Svachenko Arthur
 //  Company : Essentia
 //  Number  : 
 //  Module  : 
