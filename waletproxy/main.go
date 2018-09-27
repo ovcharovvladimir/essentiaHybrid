494e18bcff54d375bc6892ca02f5d629a1da663e
essarthur <essarthur@essentia.one>
2018-09-27 20:57:39 +0300

    Created proxy service

--- waletproxy/main.go
+++ waletproxy/main.go
@@ -1,4 +1,7 @@
+// 
 // Copyright 2018 Essentia
+// https://github.com/ovcharovvladimir/essentiaHybrid/tree/master/waletproxy
+// 
 package main
 
 import (
@@ -29,23 +32,23 @@ type Sett struct {
 
 //************************************************************
 //  Name    : Main  
-//  Date    : 25/09/2018
-//  Author  : Svachenko Arthur
-//  Company : Essentia
-//  Number  : 
-//  Module  : 
 //************************************************************
 func main() {
+
      Host  := ActiveNode()
      Sett  := ReadSettingFile()
      Port  := ":"+ Sett.Mainport
      proxy := httputil.NewSingleHostReverseProxy(&url.URL{Scheme: "http", Host:Host})
     
+    // Proxy
     proxy.Director = func(req *http.Request) {
+        fmt.Println("Host redirect:", Host) 
         req.Host       = Host
         req.URL.Host   = Host
         req.URL.Scheme = "http"    
     }
+
+
     // Proxy 
     http.Handle("/",   proxy)
     
@@ -55,6 +58,7 @@ func main() {
     http.HandleFunc("/actives/",      Active_nodes)           // Show active nodes 
     http.HandleFunc("/down/",         Down_nodes)             // Show down nodes 
     http.HandleFunc("/test/",         Api_test)               // Test service response 
+    http.HandleFunc("/admin/",        Api_admin)              // Test service response 
        
     // View settings
     fmt.Println("\n Start Service Port ", Port, "\n","Active Work Node    :", Host)
@@ -68,11 +72,6 @@ func main() {
 
 //************************************************************
 //  Name    : Show All Node 
-//  Date    : 27/09/2018
-//  Author  : Svachenko Arthur
-//  Company : Essentia
-//  Number  : 
-//  Module  : 
 //************************************************************
 func ShowNodes(w http.ResponseWriter, req *http.Request) {
     Nd:=ReadSettingFile()
@@ -80,10 +79,10 @@ func ShowNodes(w http.ResponseWriter, req *http.Request) {
     // Looop for nodes
     for _,nd:=range Nd.Nodes{
          
-         if !ChekNodeWork(nd.Ip, nd.Port){
-            nd.Status="Activate"
+         if ChekNodeWork(nd.Ip, nd.Port){
+            nd.Status="Active"
          }else{
-            nd.Status="Disabled"
+            nd.Status="Not available"
          }
          WPr(nd,w)
     }
@@ -91,11 +90,6 @@ func ShowNodes(w http.ResponseWriter, req *http.Request) {
 
 //************************************************************
 //  Name    : Show All Node 
-//  Date    : 27/09/2018
-//  Author  : Svachenko Arthur
-//  Company : Essentia
-//  Number  : 
-//  Module  : 
 //************************************************************
 func Active_node(w http.ResponseWriter, req *http.Request) {
      ft:="Active note :" + ActiveNode()
@@ -104,11 +98,6 @@ func Active_node(w http.ResponseWriter, req *http.Request) {
 
 //************************************************************
 //  Name    : Show All Node 
-//  Date    : 27/09/2018
-//  Author  : Svachenko Arthur
-//  Company : Essentia
-//  Number  : 
-//  Module  : 
 //************************************************************
 func Active_nodes(w http.ResponseWriter, req *http.Request) {
     Nd:=ReadSettingFile()
@@ -116,6 +105,7 @@ func Active_nodes(w http.ResponseWriter, req *http.Request) {
     // Looop for nodes
     for _,nd:=range Nd.Nodes{
          if ChekNodeWork(nd.Ip, nd.Port){
+            nd.Status="Active"
             WPr(nd, w)
          }
     }
@@ -123,55 +113,36 @@ func Active_nodes(w http.ResponseWriter, req *http.Request) {
 
 //************************************************************
 //  Name    : Text  output rep
-//  Date    : 26/09/2018
-//  Author  : Svachenko Arthur
-//  Company : Essentia
-//  Number  : 
-//  Module  : 
 //************************************************************
 func Wprn(Txt string, w http.ResponseWriter){
      w.Write([]byte(Txt))
 }
+
 //************************************************************
 //  Name    : Format  output rep
-//  Date    : 26/09/2018
-//  Author  : Svachenko Arthur
-//  Company : Essentia
-//  Number  : 
-//  Module  : 
 //************************************************************
 func WPr(nd Node, w http.ResponseWriter){
-     ft:="Node : "+nd.Note +"Ip : "+ nd.Ip+" Port : "+ nd.Port +" Status : " + nd.Status +" \n" 
+     ft:=fmt.Sprintf("Node :%-15s Ip : %-15s Port : %-6s Status : %-20s \n",nd.Note,nd.Ip,nd.Port,nd.Status)
      w.Write([]byte(ft))
 }
 
 //************************************************************
 //  Name    : Show All Node 
-//  Date    : 27/09/2018
-//  Author  : Svachenko Arthur
-//  Company : Essentia
-//  Number  : 
-//  Module  : 
 //************************************************************
 func Down_nodes(w http.ResponseWriter, req *http.Request) {
     Nd:=ReadSettingFile()
     // Looop for nodes
     for _,nd:=range Nd.Nodes{
-         if !ChekNodeWork(nd.Ip, nd.Port){
-            ft:="Node :"+nd.Note +"Ip:"+ nd.Ip+" Port:"+ nd.Port +" Active \n"
-            Wprn(ft,w)
-     }
          
+         if !ChekNodeWork(nd.Ip, nd.Port){
+             nd.Status ="Is Down"
+             WPr(nd,w)
+          }
     }
 }
 
 //************************************************************
 //  Name    : Test
-//  Date    : 27/09/2018
-//  Author  : Svachenko Arthur
-//  Company : Essentia
-//  Number  : 
-//  Module  : 
 //************************************************************
 func Api_test (wr http.ResponseWriter, rq *http.Request) {
      t:="Test service " + time.Now().Format("02/01/2006 15:45")
@@ -207,11 +178,6 @@ func addCORS(handler http.Handler) http.Handler {
 
 //************************************************************
 //  Name    : Get info about first active note 
-//  Date    : 25.09.2018
-//  Author  : Svachenko Arthur
-//  Company : Essentia
-//  Number  : 
-//  Module  : 
 //************************************************************
 func ActiveNode() string {
     Rip:=""
@@ -229,11 +195,6 @@ func ActiveNode() string {
 
 //************************************************************
 //  Name    : Reading  setting from file json 
-//  Date    : 26/09/2018
-//  Author  : Svachenko Arthur
-//  Company : Essentia
-//  Number  : 
-//  Module  : 
 //************************************************************
 func ReadSettingFile() Sett {
     // Setting structure
@@ -257,14 +218,8 @@ func ReadSettingFile() Sett {
     return m
 }
 
-
 //************************************************************
 //  Name    : Check node 
-//  Date    : is a snippet.
-//  Author  : Svachenko Arthr
-//  Company : Essentia
-//  Number  : 
-//  Module  : 
 //************************************************************
 func ChekNodeWork(Ip,Port string) bool {
      timeout := time.Duration(700 * time.Millisecond )
@@ -281,3 +236,80 @@ func ChekNodeWork(Ip,Port string) bool {
        return false 
     }
 }
+
+
+// *********************************************************************
+// Admin panel
+// *********************************************************************
+func Api_admin(w http.ResponseWriter, req *http.Request) {
+
+html:=`
+<!DOCTYPE html>
+<html lang="en">
+<head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Admin</title>
+
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
+
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto|Varela+Round">
+        <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+
+        <style>
+              body {color: #060f21b5; font-family: 'Roboto';  font-size: 16px;}
+              .card-header1 {background-color: #108c84; color:white; font-weight:bold;}
+        </style>
+</head>
+
+<body>
+    <div class="container">
+        <h2>Proxy inform panel</h2>
+
+        <!-- Grey with black text -->
+        <nav class="navbar navbar-expand-sm bg-dark navbar-dark">
+              <ul class="navbar-nav">
+                <li class="nav-item active"> <a class="nav-link" href="#">Active</a></li>
+                <li class="nav-item"> <a class="nav-link" href="#">Link</a> </li>
+                <li class="nav-item"> <a class="nav-link" href="#">Link</a> </li>
+                <li class="nav-item"> <a class="nav-link disabled" href="#">Disabled</a> </li>
+              </ul>
+        </nav>
+        
+
+        <!--
+            <div class="card bg-info text-white">
+                <div class="card-body">Proxy inform panel</div>
+            </div>
+        -->
+
+        <br>
+        <div class="card">
+                    <div class="card-header" style="background-color:#red;">
+                         <h5>Admin panel</h5>
+                    </div>
+
+                    <div class="card-body">
+                          <a href="/nodes/">Preview all nodes</a><br>
+                          <a href="/active/">Preview <b>first</b> active node</a><br>
+                          <a href="/actives">Preview all <b>active</b> nodes</a><br>
+                          <a href="/down/">Preview all <b>Disabled</b>nodes</a><br>
+                          <a href="/test/">Test service</a><br>
+                    </div>      
+                    <div class="card-footer">Essentia</div>           
+                </div>
+
+        </div>    
+    </div>    
+</body>
+</html>
+ `
+Wprn(html,w)
+}
+
+
