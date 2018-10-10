fa1ca1505b251af05718337276859d746433f5f3
Vladimir Ovcharov <vladimirovcharov@essentia.one>
2018-10-10 15:12:21 -0400

    *fixed "missing trie node" error

--- params/config.go
+++ params/config.go
@@ -40,7 +40,7 @@ var (
 		EIP150Block:    big.NewInt(20),
 		EIP150Hash:     common.HexToHash("0x2086799aeebeae135c246c65021c82b4e15a2c451340993aacfd2751886514f0"),
 		EIP155Block:    big.NewInt(500),
-		EIP158Block:    big.NewInt(500),
+		EIP158Block:    big.NewInt(1),
 
 		ByzantiumBlock: big.NewInt(1700000),
 
